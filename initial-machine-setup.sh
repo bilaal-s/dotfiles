5a0e4428a05a0f484db7caafda20e2e77b1fb90c
Bilaal Salad <bilaaldev@gmail.com>
2019-10-24 13:57:08 +0100

    adds mpd and ncmpcpp config, also ammends i3 and polybar configs.

--- initial-machine-setup.sh
+++ initial-machine-setup.sh
@@ -243,3 +243,51 @@ sudo apt install rxvt-unicode
 
 #font config
 sudo ln -s ~/dotfiles/fonts/conf.d/10-ab-Iosevka.conf /etc/fonts/conf.d/ 
+
+#mpd
+#download release tar and cd into untarred folder
+sudo apt-get install g++ \
+  libpcre3-dev \
+  libmad0-dev libmpg123-dev libid3tag0-dev \
+  libflac-dev libvorbis-dev libopus-dev \
+  libadplug-dev libaudiofile-dev libsndfile1-dev libfaad-dev \
+  libfluidsynth-dev libgme-dev libmikmod2-dev libmodplug-dev \
+  libmpcdec-dev libwavpack-dev libwildmidi-dev \
+  libsidplay2-dev libsidutils-dev libresid-builder-dev \
+  libavcodec-dev libavformat-dev \
+  libmp3lame-dev libtwolame-dev libshine-dev \
+  libsamplerate0-dev libsoxr-dev \
+  libbz2-dev libcdio-paranoia-dev libiso9660-dev libmms-dev \
+  libzzip-dev \
+  libcurl4-gnutls-dev libyajl-dev libexpat-dev \
+  libasound2-dev libao-dev libjack-jackd2-dev libopenal-dev \
+  libpulse-dev libshout3-dev \
+  libsndio-dev \
+  libmpdclient-dev \
+  libnfs-dev libsmbclient-dev \
+  libupnp-dev \
+  libavahi-client-dev \
+  libsqlite3-dev \
+  libsystemd-dev \
+  libgtest-dev \
+  libboost-dev \
+  libicu-dev
+# note: local bin meson is because it was installed with pip3
+~/.local/bin/meson . output/release --buildtype=debugoptimized -Db_ndebu=true
+ninja -C output/release 
+sudo ninja -C output/release install
+mkdir ~/.config/mpd
+ln -s ~/dotfiles/mpd/mpd.conf ~/.config/mpd
+mkdir ~/quran
+mkdir ~/quran/playlists
+#once m~/.config/mpd check if files like db and pid file are autgenerated, if so just save the config and leave the other files and if not autgenerated copy the whole mpd config dir to dotfiles and symlink
+
+#ncmpcpp
+# get release tar and untar
+./autogen.sh 
+sudo apt-get install libboost-date-time-dev libboost-all-dev libreadline-dev libncursesw5-dev
+./configure --enable-outputs --enable-visualizer --enable-clock
+make
+sudo make install
+mkdir ~/.config/ncmpcpp
+ln -s ~/dotfiles/ncmpcpp/config ~/.config/ncmpcpp
