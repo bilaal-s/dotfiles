c78c8c01bf298dfb44887ff25c7b3d4e5001eec0
Bilaal Salad <bilaaldev@gmail.com>
2020-03-03 19:08:08 +0000

    update.

--- scripts/fix-ovpn.sh
+++ scripts/fix-ovpn.sh
@@ -3,3 +3,7 @@
 sudo mkdir -p /etc/openvpn/scripts
 sudo wget https://raw.githubusercontent.com/jonathanio/update-systemd-resolved/master/update-systemd-resolved -P /etc/openvpn/scripts/
 sudo chmod +x /etc/openvpn/scripts/update-systemd-resolved
+
+# the following steps were taken:
+# sudo rm /etc/resolv.conf
+# sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
